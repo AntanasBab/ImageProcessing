1497a18834dff2795d2c67081c31c40dd07e0a96
AntanasB <antanasbabich@gmail.com>
2024-10-05 21:00:19 +0300

    privet pupsik

---
+++ CA1/IntensityTransformations/histogramStretching.py
@@ -0,0 +1,46 @@
+import sys
+import numpy as np
+from libtiff import TIFF
+from matplotlib import pyplot as plt
+
+def load_image(image_path):
+    tifImg = TIFF.open(image_path)
+    image = TIFF.read_image(tifImg)
+    tifImg.close()
+    return image
+
+def compute_stretch(image, s_min=0, s_max=255):
+    r_min = np.min(image)
+    r_max = np.max(image)
+    
+    if r_max == r_min:
+        return np.full(image.shape, s_min if r_max < 128 else s_max, dtype=np.uint8)
+    
+    stretched = (image - r_min) / (r_max - r_min) * (s_max - s_min) + s_min - 10
+    return stretched
+
+def apply_stretch(image):
+    stretched_image = compute_stretch(image)
+    stretched_image = np.clip(stretched_image, 0, 255).astype(np.uint8)
+    return stretched_image
+
+def main(image_path):
+    image = load_image(image_path)
+    stretched_image = apply_stretch(image)
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Original Image")
+    plt.imshow(image, cmap='gray', vmin=0, vmax=255)
+    
+    plt.subplot(1, 2, 2)
+    plt.title("Stretched Image")
+    plt.imshow(stretched_image, cmap='gray', vmin=0, vmax=255)
+    
+    plt.show()
+
+if len(sys.argv) != 2:
+    print("Usage: python3 ex2.py <image_path>")
+    sys.exit(1)
+
+main(sys.argv[1])
---
+++ CA1/IntensityTransformations/thresholding.py
@@ -0,0 +1,43 @@
+import sys
+import numpy as np
+from libtiff import TIFF
+from matplotlib import pyplot as plt
+
+def load_image(image_path):
+    tifImg = TIFF.open(image_path)
+    image = TIFF.read_image(tifImg)
+    tifImg.close()
+    return image
+
+def compute_threshold(image, threshold_value=128):
+    binary_image = np.where(image > threshold_value, 255, 0)
+    return binary_image
+
+def apply_threshold(image, threshold_value=128):
+    thresholded_image = compute_threshold(image, threshold_value)
+    thresholded_image = thresholded_image.astype(np.uint8)
+    return thresholded_image
+
+def main(image_path, threshold_value):
+    image = load_image(image_path)
+    
+    thresholded_image = apply_threshold(image, threshold_value)
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Original Image")
+    plt.imshow(image, cmap='gray', vmin=0, vmax=255)
+    
+    plt.subplot(1, 2, 2)
+    plt.title(f"Thresholded Image (Threshold = {threshold_value})")
+    plt.imshow(thresholded_image, cmap='gray', vmin=0, vmax=255)
+    
+    plt.show()
+
+if len(sys.argv) != 3:
+    print("Usage: python script.py <image_path> <threshold_value>")
+    sys.exit(1)
+
+image_path = sys.argv[1]
+threshold_value = int(sys.argv[2])
+main(image_path, threshold_value)
